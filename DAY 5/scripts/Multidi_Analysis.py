b1a1d57d2e1751d67bd38f82ea31aa790cb55fbb
tharun28-ch <tharunguru28@gmail.com>
2026-04-10 18:47:03 +0530

    DAY5 Completed

--- DAY 5/scripts/Multidi_Analysis.py
+++ DAY 5/scripts/Multidi_Analysis.py
@@ -3,6 +3,5 @@ df = pd.read_csv("../data/sales.csv")
 
 pivot = df.pivot_table(values='Sales', index='productID', aggfunc='sum')
 pivot.to_csv("../data/products.csv")
-
 print("--- Product Performance Pivot ---")
 print(pivot)
